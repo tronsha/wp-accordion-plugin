213173342694db2099f2e66446520894d6f36351
Stefan Hüsges <Tronsha@gmx.de>
2014-11-03 15:06:53 +0100

    Update accordion.js

--- js/accordion.js
+++ js/accordion.js
@@ -1,13 +1,13 @@
-jQuery(document).ready(function() {
+jQuery(document).ready(function () {
     var $ = jQuery;
-    var open = false;
-    $('.accordion').child('h2, h3, h4, h5, h6').each(function() {
+    var open = true;
+    $('.accordion').child('h2, h3, h4, h5, h6, strong').each(function () {
         var $this = $(this);
-        $this.bind('click', function() {
+        $this.bind('click', function () {
             var $div = $this.next('div');
             if ($div.css('height') == '0px') {
                 if (open) {
-                    $(this).parent(.accordion).child('div').each(function() {
+                    $(this).parent('.accordion').child('div').each(function () {
                         $(this).animate({height: 0}, 1000);
                     });
                 }
@@ -15,7 +15,7 @@ jQuery(document).ready(function() {
                 $div.css('height', 'auto');
                 var autoHeight = $div.height();
                 $div.css('height', '0');
-                $div.animate({height: autoHeight}, 1000, function() {
+                $div.animate({height: autoHeight}, 1000, function () {
                     $div.css('height', 'auto');
                 });
             }
