28c47b0030844a334b61c81dec51c7a7cf6ce2eb
Stefan Hüsges <tronsha@gmx.de>
2016-03-02 08:03:33 +0100

    vendor yuicompressor

--- minify.sh
+++ minify.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
-wget https://github.com/yui/yuicompressor/releases/download/v2.4.8/yuicompressor-2.4.8.jar
-java -jar yuicompressor-2.4.8.jar --type js ./src/js/mpcx-accordion.js -o ./mpcx-accordion/trunk/public/js/mpcx-accordion.min.js
-java -jar yuicompressor-2.4.8.jar --type css ./src/css/mpcx-accordion.css -o ./mpcx-accordion/trunk/public/css/mpcx-accordion.min.css
-rm ./yuicompressor-2.4.8.jar
+java -jar ./vendor/yuicompressor-2.4.8.jar --type js ./src/js/mpcx-accordion.js -o ./mpcx-accordion/trunk/public/js/mpcx-accordion.min.js
+java -jar ./vendor/yuicompressor-2.4.8.jar --type css ./src/css/mpcx-accordion.css -o ./mpcx-accordion/trunk/public/css/mpcx-accordion.min.css
