7caef58f3c90f0b02f2dbbd86d408deadaaf1334
Stefan Hüsges <tronsha@gmx.de>
2016-03-02 08:07:34 +0100

    rename directory

--- minify.sh
+++ minify.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-java -jar ./vendor/yuicompressor-2.4.8.jar --type js ./src/js/mpcx-accordion.js -o ./mpcx-accordion/trunk/public/js/mpcx-accordion.min.js
-java -jar ./vendor/yuicompressor-2.4.8.jar --type css ./src/css/mpcx-accordion.css -o ./mpcx-accordion/trunk/public/css/mpcx-accordion.min.css
+java -jar ./vendor/yuicompressor-2.4.8.jar --type js ./src/js/mpcx-accordion.js -o ./wordpress/trunk/public/js/mpcx-accordion.min.js
+java -jar ./vendor/yuicompressor-2.4.8.jar --type css ./src/css/mpcx-accordion.css -o ./wordpress/trunk/public/css/mpcx-accordion.min.css
