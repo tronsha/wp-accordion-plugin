1f6dddf3b61464d91d6f592eaa94d79748ce5b17
Stefan Hüsges <Tronsha@gmx.de>
2016-08-31 16:36:55 +0200

    Update mpcx-accordion.php

--- wordpress/trunk/mpcx-accordion.php
+++ wordpress/trunk/mpcx-accordion.php
@@ -8,7 +8,7 @@
  * Plugin Name:       Accordion
  * Plugin URI:        https://github.com/tronsha/wp-accordion-plugin
  * Description:       Just an Accordion Plugin.
- * Version:           1.1.7
+ * Version:           1.2.0-beta
  * Author:            Stefan Hüsges
  * Author URI:        http://www.mpcx.net/
  * Copyright:         Stefan Hüsges
@@ -17,6 +17,13 @@
  */
 
 defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
+
+register_activation_hook(
+	__FILE__,
+	function () {
+		add_option( 'mpcx-accordion', '[]' );
+	}
+);
 
 if ( ! is_admin() ) {
 
@@ -27,13 +34,13 @@ if ( ! is_admin() ) {
 				'mpcx-accordion',
 				plugin_dir_url( __FILE__ ) . 'public/css/accordion.min.css',
 				array(),
-				'1.1.7'
+				'1.2.0'
 			);
 			wp_register_script(
 				'mpcx-accordion',
 				plugin_dir_url( __FILE__ ) . 'public/js/accordion.min.js',
 				array( 'jquery' ),
-				'1.1.7'
+				'1.2.0'
 			);
 			wp_enqueue_style( 'mpcx-accordion' );
 			wp_enqueue_script( 'mpcx-accordion' );
@@ -43,7 +50,15 @@ if ( ! is_admin() ) {
 	add_shortcode(
 		'accordion',
 		function ( $att = array(), $content = null ) {
-			$content = do_shortcode( $content );
+			if ( isset( $att['id'] ) === true ) {
+				$accordion = json_decode( get_option( 'mpcx-accordion' ), true );
+				$content = '';
+				foreach ( $accordion[ $att['id'] ]['data'] as $data ) {
+					$content .= '<h3>' . $data['headline'] . '</h3><div>' . $data['text'] . '</div>';
+				}
+			} else {
+				$content = do_shortcode( $content );
+			}
 			return '<div class="accordion">' . $content . '</div>';
 		}
 	);
