d3c7e1a96f471027b36672aae5640a7687995a8f
Stefan Hüsges <tronsha@gmx.de>
2016-09-02 12:29:51 +0200

    update

--- wordpress/trunk/admin/options.php
+++ wordpress/trunk/admin/options.php
@@ -45,22 +45,38 @@ $accordions = json_decode( get_option( 'mpcx-accordion' ), true );
 	<div class="wrap">
 		<h1>Accordion</h1>
 		<form method="post" action="admin.php?page=accordion&amp;id=<?php echo $_GET['id']; ?>">
+			<?php submit_button(); ?>
+			<?php
+			$count = count( $accordions[ $_GET['id'] ]['data'] );
+			if ( isset( $_POST['submit'] ) === true ) {
+				$data = [ ];
+				for ( $i = 0; $i < $count; $i ++ ) {
+					$data[ $i ]['headline'] = $_POST['headline'][ $i ];
+					$data[ $i ]['text']     = $_POST['text'][ $i ];
+				}
+				$accordions[ $_GET['id'] ]['data'] = $data;
+				update_option( 'mpcx-accordion', json_encode( $accordions ) );
+			}
+			$i = 1;
+			?>
 			<table class="form-table">
+				<?php $i = 1; ?>
 				<?php foreach ( $accordions[ $_GET['id'] ]['data'] as $key => $data ): ?>
 					<tr>
-						<th>Headline</th>
+						<td style="vertical-align: top; font-weight: bold;">
+							<h2><?php echo $i; ?>.)</h2>
+						</td>
 						<td>
-							<input type="text" id="headline_<?php echo $key; ?>" name="headline_<?php echo $key; ?>" value="<?php echo htmlentities( $data['headline'] ); ?>" style="width: 100%;"/>
+							<h3>Headline</h3>
+							<input type="text" id="headline_<?php echo $key; ?>" name="headline[<?php echo $key; ?>]" value="<?php echo htmlentities( $data['headline'] ); ?>" style="width: 100%;"/>
+							<h3>Text</h3>
+							<textarea id="text_<?php echo $key; ?>" name="text[<?php echo $key; ?>]" rows="10" style="width: 100%;"><?php echo htmlentities( $data['text'] ); ?></textarea>
 						</td>
 					</tr>
-					<tr>
-						<th>Text</th>
-						<td>
-							<textarea id="text_<?php echo $key; ?>" name="text_<?php echo $key; ?>" rows="10" style="width: 100%;"><?php echo htmlentities( $data['text'] ); ?></textarea>
-						</td>
-					</tr>
+					<?php $i++; ?>
 				<?php endforeach; ?>
 			</table>
+			<input type="hidden" name="count" value="<?php echo $count; ?>">
 			<?php submit_button(); ?>
 		</form>
 	</div>
