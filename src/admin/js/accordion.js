900972bdf2b4ad8710818daa4e436d57108d9008
Stefan Hüsges <tronsha@gmx.de>
2016-11-15 18:45:12 +0100

    update

--- src/admin/js/accordion.js
+++ src/admin/js/accordion.js
@@ -1,38 +1,38 @@
 jQuery(document).ready(function () {
 
-    updateOutput();
+    updateAccordionEditOutput();
 
     jQuery('input[type="text"], textarea').bind('change keyup', function () {
-        bindBeforeunload();
+        bindAccordionBeforeunload();
     });
 
     jQuery('input[type="submit"]').bind('click', function () {
-        unbindBeforeunload();
+        unbindAccordionBeforeunload();
     });
 
     jQuery('[data-direction="down"]').bind('click', function () {
-        moveDown(jQuery(this).parents('table').data('position'));
-        bindBeforeunload();
+        moveAccordionDataDown(jQuery(this).parents('table').data('position'));
+        bindAccordionBeforeunload();
     });
 
     jQuery('[data-direction="up"]').bind('click', function () {
-        moveUp(jQuery(this).parents('table').data('position'));
-        bindBeforeunload();
+        moveAccordionDataUp(jQuery(this).parents('table').data('position'));
+        bindAccordionBeforeunload();
     });
 
 });
 
-function bindBeforeunload() {
+function bindAccordionBeforeunload() {
     jQuery(window).bind('beforeunload', function () {
         return '';
     });
 }
 
-function unbindBeforeunload() {
+function unbindAccordionBeforeunload() {
     jQuery(window).unbind('beforeunload');
 }
 
-function move(from, to) {
+function moveAccordionData(from, to) {
     var selectorHeadlineFrom = jQuery('[data-position="' + from + '"] [data-type="headline"]');
     var selectorHeadlineTo = jQuery('[data-position="' + to + '"] [data-type="headline"]');
     var selectorTextFrom = jQuery('[data-position="' + from + '"] [data-type="text"]');
@@ -45,18 +45,18 @@ function move(from, to) {
     selectorTextFrom.val(tmpText);
 }
 
-function moveUp(position) {
-    move(position, position - 1);
+function moveAccordionDataUp(position) {
+    moveAccordionData(position, position - 1);
 }
 
-function moveDown(position) {
-    move(position, position + 1);
+function moveAccordionDataDown(position) {
+    moveAccordionData(position, position + 1);
 }
 
-function updateOutput() {
+function updateAccordionEditOutput() {
     var positionCounter = 1;
-    var $formtable = jQuery('table.form-table');
-    $formtable.each(function () {
+    var selectorFormTable = jQuery('table.form-table');
+    selectorFormTable.each(function () {
         var $this = jQuery(this);
         $this.attr('data-position', positionCounter);
         if (positionCounter === 1) {
@@ -64,7 +64,7 @@ function updateOutput() {
         } else {
             $this.find('[data-direction="up"]').css('display', '');
         }
-        if (positionCounter === $formtable.length) {
+        if (positionCounter === selectorFormTable.length) {
             $this.find('[data-direction="down"]').css('display', 'none');
         } else {
             $this.find('[data-direction="down"]').css('display', '');
