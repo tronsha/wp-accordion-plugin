dcd07db23879af4710aaf91b84c0ea43d500b6b8
Stefan Hüsges <tronsha@gmx.de>
2016-11-18 12:07:38 +0100

    update

--- wordpress/trunk/mpcx-accordion.php
+++ wordpress/trunk/mpcx-accordion.php
@@ -63,7 +63,7 @@ if ( ! is_admin() ) {
 				$content = do_shortcode( $content );
 			}
 
-			return '<div class="accordion">' . $content . '</div>';
+			return '<div class="accordion"' . (intval($att['id']) > 0 ? ' id="accordion-' . $att['id'] . '"' : '') . '>' . $content . '</div>';
 		}
 	);
 
