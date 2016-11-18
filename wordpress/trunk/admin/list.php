07169e2f81685036a8411138b4150789387092ca
Stefan Hüsges <tronsha@gmx.de>
2016-11-18 11:54:23 +0100

    update

--- wordpress/trunk/admin/list.php
+++ wordpress/trunk/admin/list.php
@@ -38,21 +38,21 @@ $post_type_object = get_post_type_object( 'page' );
 				continue;
 			} ?>
 			<tr>
-				<td class="title column-title column-primary">
+				<td class="column-title column-primary">
 					<strong>
 						<a class="row-title" href="<?php echo admin_url( 'admin.php?page=accordion&amp;edit=' . $key ); ?>"><?php echo $accordion['title']; ?></a>
 					</strong>
 					<button type="button" class="toggle-row"></button>
 				</td>
-				<td class="shortcode column-shortcode" data-colname="<?php _e( 'Shortcode', 'mpcx-accordion' ); ?>">
+				<td class="column-shortcode" data-colname="<?php _e( 'Shortcode', 'mpcx-accordion' ); ?>">
 					[accordion id="<?php echo $key; ?>" /]
 				</td>
-				<td class="options column-edit" data-colname="<?php _e( 'Edit', 'mpcx-accordion' ); ?>">
+				<td class="column-edit" data-colname="<?php _e( 'Edit', 'mpcx-accordion' ); ?>">
 					<a href="<?php echo admin_url( 'admin.php?page=accordion&amp;edit=' . $key ); ?>" title="<?php _e( 'Edit', 'mpcx-accordion' ); ?>">
 						<span class="dashicons dashicons-edit"></span>
 					</a>
 				</td>
-				<td class="options column-delete" data-colname="<?php _e( 'Delete', 'mpcx-accordion' ); ?>">
+				<td class="column-delete" data-colname="<?php _e( 'Delete', 'mpcx-accordion' ); ?>">
 					<a href="<?php echo admin_url( 'admin.php?page=accordion&amp;delete=' . $key ); ?>" title="<?php _e( 'Delete', 'mpcx-accordion' ); ?>" onclick="return confirm('<?php printf( __( 'Are you sure you want to delete %s?', 'mpcx-accordion' ), $accordion['title'] ); ?>');">
 						<span class="dashicons dashicons-trash"></span>
 					</a>
